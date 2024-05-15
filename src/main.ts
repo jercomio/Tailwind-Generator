f042569417a3ec90e8d8e3546397acab1512f38d
jercomio <joelroure01@gmail.com>
2024-05-14 22:27:09 -0400

    TG@v0.0.1a

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+const form = document.querySelector('#generate-form') as HTMLFormElement;
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const prompt = formData.get('prompt') as string
+
+  alert(prompt);
+});
